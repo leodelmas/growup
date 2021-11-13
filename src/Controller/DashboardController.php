9a428dd028b9f243c9cc7992fced2d7fd0358a76
Léo Delmas <ldelmaspro@gmail.com>
2021-11-13 11:33:43 +0100

    DashboardController name

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ExerciseController extends AbstractController
+class DashboardController extends AbstractController
 {
     #[Route('/', name: 'dashboard_index', methods: ['GET'])]
     public function index(): Response
