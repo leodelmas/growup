7aef90dea548497974b4d930dad130e7d8beaf65
Léo Delmas <ldelmaspro@gmail.com>
2021-12-08 09:54:05 +0100

    Delete series

--- assets/app.js
+++ assets/app.js
@@ -12,11 +12,30 @@ import './scss/app.scss';
 import './bootstrap';
 
 // TODO : Découpage JS
-const el = document.querySelector("button.add_item_link");
-el.addEventListener("click", function(e) {
+const addItemLink = document.querySelector("button.add_item_link");
+addItemLink.addEventListener("click", function(e) {
     const collectionHolder = document.querySelector('.' + e.currentTarget.dataset.collectionHolderClass);
     const item = document.createElement('li');
     item.innerHTML = collectionHolder.dataset.prototype.replace(/__name__/g, collectionHolder.dataset.index);
     collectionHolder.appendChild(item);
+    addTagFormDeleteLink(item)
     collectionHolder.dataset.index ++;
 }, false);
+
+const seriesList = (document.querySelector('ul.series')).getElementsByTagName('li')
+for (let series of seriesList) {
+    addTagFormDeleteLink(series)
+}
+
+function addTagFormDeleteLink (tagFormLi) {
+    const removeFormButton = document.createElement('button')
+    removeFormButton.classList.add('text-indigo-500')
+    removeFormButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewbox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/></svg>'
+
+    tagFormLi.append(removeFormButton);
+
+    removeFormButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        tagFormLi.remove();
+    });
+}
