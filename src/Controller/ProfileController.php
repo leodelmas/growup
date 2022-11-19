d31816d104e71bf9a7dc4e0a4c0156b068d3b29a
leodelmas <ldelmaspro@gmail.com>
2022-11-19 23:13:56 +0100

    :construction: Profile index and profile editor

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
@@ -11,14 +13,35 @@ use Symfony\Component\Security\Core\Security;
 #[Route('/profile')]
 class ProfileController extends AbstractController
 {
-    #[Route('/{user}', name: 'profile_index', methods: ['GET'], defaults: ["user" => null])]
+    #[Route('/', name: 'profile_index', methods: ['GET'])]
     public function index(User $user = null, Security $security): Response
     {
         /** @var User */
-        $user = $user ?: $security->getUser();
+        $user = $security->getUser();
         return $this->render('profile/index.html.twig', [
             'user' => $user,
             'lastSessions' => $user->getLastSessions(2)
+        ]);
+    }
+
+    #[Route('/edit', name: 'profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Security $security): Response
+    {
+        /** @var User */
+        $user = $security->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('profile_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profile/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
         ]);
     }
 }
