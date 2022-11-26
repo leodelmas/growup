9b2730fd9d3efd3935e8fd375746bd5734fc3654
leodelmas <ldelmaspro@gmail.com>
2022-11-26 21:49:46 +0100

    :construction: RecordedExercise, add description field

--- src/Entity/RecordedExercise.php
+++ src/Entity/RecordedExercise.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\RecordedExerciseRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: RecordedExerciseRepository::class)]
@@ -15,6 +16,9 @@ class RecordedExercise
 
     #[ORM\Column(type: 'string', length: 255)]
     private $name;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
 
     public function getId(): ?int
     {
@@ -29,6 +33,18 @@ class RecordedExercise
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
